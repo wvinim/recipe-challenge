051f2f7c2bddd09b7383f9d7eb1a890a3a1e90e4
Vinicius Moraes <wvinim@msn.com>
2020-10-15 09:51:12 -0300

    add comments

--- src/controller/RecipesController.ts
+++ src/controller/RecipesController.ts
@@ -7,19 +7,27 @@ import RecipePuppyRepository from '../repositories/RecipePuppyRepository';
 class RecipesController {
   async search(keywords: string[]): Promise<Recipe[] | null> {
     const giphyHelper = new GiphyRepository();
-
     const recipePuppyHelper = new RecipePuppyRepository();
+
+    /** Get recipespuppy data */
     const recipesPuppy = await recipePuppyHelper.search(keywords);
+
+    /** Service unavailable */
     if (!recipesPuppy) {
       return null;
     }
     const recipes: Recipe[] = [];
 
     for (const element of recipesPuppy) {
+      /** Get giphy url */
       const gif = await giphyHelper.search(element.title);
+
+      /** Service unavailable */
       if (!gif) {
         return null;
       }
+
+      /** Make recipe object */
       const recipe = new Recipe(
         element.title,
         element.ingredients.split(', ').sort(),
--- src/routes/recipes.routes.ts
+++ src/routes/recipes.routes.ts
@@ -8,16 +8,19 @@ recipesRouter.get('/', async (request, response) => {
   const recipeController = new RecipeController();
   let params: string[] = [];
 
+  /** Making params by request */
   if ('i' in request.query) {
     params = request.query.i.toString().split(',');
   }
 
+  /** Check params */
   if (params.length < 1) {
     response.status(400).json({
       error: '400 BAD REQUEST: Please, verify your ingredients.',
     });
   }
 
+  /** Check max params */
   if (params.length > 3) {
     response.status(400).json({
       error:
@@ -25,8 +28,10 @@ recipesRouter.get('/', async (request, response) => {
     });
   }
 
+  /** Call main search controller */
   const recipes = await recipeController.search(params);
 
+  /** Service unavailable */
   if (!recipes) {
     response.status(400).json({
       error: 'Service unavailable',
